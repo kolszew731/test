778f7d7c5e9d173f068625ac40a1fe91bf90acf1
Krzysztof Olszewski <kolszew73@gmail.com>
2015-08-27 14:36:39 +0200

    dodanie

--- TestGit/src/pl/test/MainClass.java
+++ TestGit/src/pl/test/MainClass.java
@@ -4,6 +4,10 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("aaaaa bbb");
+		System.out.println("aaaaa bbb");
+		System.out.println("aaaaa bbb");
+		System.out.println("aaaaa bbb");
+		System.out.println("aaaaa bbb");
 	}
 
 }
---
+++ TestGit/src/pl/test/Runnable.java
@@ -0,0 +1,5 @@
+package pl.test;
+
+public interface Runnable {
+
+}
