d8cc6fef069288fadaeaef2f4c2026d169e0eebe
Krzysztof Olszewski <kolszew73@gmail.com>
2015-08-28 22:15:18 +0200

    aaaaa

--- HelloWorld/src/pl/test/MainClass.java
+++ HelloWorld/src/pl/test/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	private final static int X = 0;
 
 	public static void main(String[] args) {
-		System.out.println("Ala ma kota ... ..."); 
+		System.out.println("Ala ma kota ... ..." + X); 
 		System.exit(1);
 	}
 
